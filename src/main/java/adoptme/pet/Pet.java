f7f39e216a961e58139128a536ddd2cae868df46
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-05-03 15:31:18 -0500

    setup pet class

---
+++ src/main/java/adoptme/pet/Pet.java
@@ -0,0 +1,38 @@
+package adoptme.pet;
+
+public abstract class Pet {
+	private String name;
+	private String species;
+	private int age;
+	
+	public Pet() { // default constructor
+		this.name = null;
+		this.species = null;
+		this.age = -1;
+	}
+	
+	public Pet(String n, String s, int a) {
+		this.name = n;
+		this.species = s;
+		this.age = a;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSpecies() {
+		return species;
+	}
+	public void setSpecies(String species) {
+		this.species = species;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+}
