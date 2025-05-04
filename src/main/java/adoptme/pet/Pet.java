2df3e19189d69767a07142e1791963312f1ba394
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-05-04 15:50:07 -0500

    Added javadoc code to Pet.java file.

--- src/main/java/adoptme/pet/Pet.java
+++ src/main/java/adoptme/pet/Pet.java
@@ -1,6 +1,10 @@
 package adoptme.pet;
 
 public abstract class Pet {
+	
+	/**
+	 * Fields that will be used within the specific pet classes.
+	 */
 	protected int id;
 	protected String name;
 	protected String species;
@@ -8,6 +12,9 @@ public abstract class Pet {
 	
 	protected boolean isAdopted;
 	
+	/**
+	 * toString for Pet class.
+	 */
 	@Override
 	public String toString() {
 		String petString = "Name: " + this.name + " ID: " + this.id + " Species: " + this.species + " Age: " + this.age;
@@ -15,6 +22,10 @@ public abstract class Pet {
 		return petString;
 	}
 	
+	/**
+	 * Abstract getter/setter methods for fields within the Pet class.
+	 * @return
+	 */
 	public abstract String getName();
 	public abstract String getSpecies();
 	public abstract int getAge();
