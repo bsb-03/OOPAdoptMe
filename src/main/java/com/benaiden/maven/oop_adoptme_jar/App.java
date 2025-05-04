b4dc63df9972d79f9b0d544c53f13080885aaf7f
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-05-03 19:20:07 -0500

    added todo pseudocode to App.java

--- src/main/java/com/benaiden/maven/oop_adoptme_jar/App.java
+++ src/main/java/com/benaiden/maven/oop_adoptme_jar/App.java
@@ -6,6 +6,14 @@ public class App
 {
     public static void main( String[] args )
     {
+    	/*
+    	 * PSEUDOCODE
+    	 * Make new shelter
+    	 * Fill shelter with data from .json files
+    	 * Open interface, display info
+    	 * Interface buttons (add, delete, adopt, etc.)
+    	 * On close: fill .json with udated data
+    	 */
         Pet myPet = new Dog("Spot", "Boston Terrier", 5);
         
         System.out.println(myPet.toString());
