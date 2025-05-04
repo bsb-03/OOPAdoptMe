281eef71fc54ca68be1faea334a823fc6d269218
Nunya Bidnis <benjamin.beck2003@gmail.com>
2025-05-04 16:23:05 -0500

    .json file parsing added to App.java

--- src/main/java/com/benaiden/maven/oop_adoptme_jar/App.java
+++ src/main/java/com/benaiden/maven/oop_adoptme_jar/App.java
@@ -1,37 +1,50 @@
 package com.benaiden.maven.oop_adoptme_jar;
+
+import java.io.*;
+import java.util.List;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.typeadapters.RuntimeTypeAdapterFactory;
+
 import adoptme.pet.*;
 import adoptme.shelter.*;
 
+public class App {
+    public static void main(String[] args) {
+        String filename = "./src/main/resources/pets.json";
+        Shelter<Pet> petShelter = new Shelter<>();
 
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	/*
-    	 * PSEUDOCODE
-    	 * Make new shelter
-    	 * Fill shelter with data from .json files
-    	 * Open interface, display info
-    	 * Interface buttons (add, delete, adopt, etc.)
-    	 * On close: fill .json with updated data
-    	 */
-    	/* OLD TEST CODE
-    	 * 
-    	Shelter myShelter = new Shelter<Pet>();
-    	
-        Pet pet1 = new Dog("Spot", "Boston Terrier", 5, false);
-        Pet pet2 = new Cat("Sally", "Maine Coon", 2, false);
-        Pet pet3 = new Rabbit("Arnold", "White", 1, false);
+        RuntimeTypeAdapterFactory<Pet> typeFactory = RuntimeTypeAdapterFactory
+                .of(Pet.class, "type") // type field determines pet type
+                .registerSubtype(Cat.class, "Cat")
+                .registerSubtype(Dog.class, "Dog")
+                .registerSubtype(Rabbit.class, "Rabbit");
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapterFactory(typeFactory)
+                .create();
+
+        JsonReader reader;
+        try {
+            reader = new JsonReader(new FileReader(filename));
+        } catch (FileNotFoundException e) {
+            System.out.println("FILE NOT FOUND");
+            return;
+        }
+
+        try {
+            List<Pet> pets = gson.fromJson(reader, new TypeToken<List<Pet>>(){}.getType());
+            for (Pet pet : pets) {
+                petShelter.addPet(pet);
+            }
+            System.out.println("Loaded " + pets.size() + " pets into the shelter.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         
-        myShelter.addPet(pet1);
-        myShelter.addPet(pet2);
-        myShelter.addPet(pet3);
-        
-        myShelter.sortByName();
-        
-        System.out.println(myShelter.toString());*/
-    	
-    	
-        
+        System.out.println(petShelter.toString());
     }
 }
