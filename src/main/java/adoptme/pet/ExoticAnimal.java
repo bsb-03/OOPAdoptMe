85eb669145c967985b078e722d23cce300e33412
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-05-04 20:20:06 -0500

    Added javadoc code to ExoticAnimal.java

--- src/main/java/adoptme/pet/ExoticAnimal.java
+++ src/main/java/adoptme/pet/ExoticAnimal.java
@@ -2,7 +2,14 @@ package adoptme.pet;
 
 public class ExoticAnimal extends Pet {
     private String category;
-
+    
+    /**
+     * Parameterized constructor for ExoticAnimal class
+     * @param id - Pet ID
+     * @param name - Pet name
+     * @param species - Pet species
+     * @param age - Pet age
+     */
     public ExoticAnimal(String id, String name, String species, int age) {
         this.id = Integer.parseInt(id.replaceAll("\\D+", "")); // convert "exo001" → 1
         this.name = name;
@@ -11,23 +18,39 @@ public class ExoticAnimal extends Pet {
         this.category = category;
         this.isAdopted = false;
     }
-
+    
+    /**
+     * Setter/adopter method for isAdopted field
+     */
     public void adopt() {
         this.isAdopted = true;
     }
-
+    
+    /**
+     * Getter method for category field.
+     * @return
+     */
     public String getCategory() {
         return category;
     }
     
+    /**
+     * Getter method for name field.
+     */
     public String getName() {
     	return name;
     }
     
+    /**
+     * Getter method for species field.
+     */
     public String getSpecies() {
     	return species;
     }
     
+    /**
+     * Getter method for age field.
+     */
     public int getAge() {
     	return age;
     }
