0e7c12cfc40a5ac83eab5a76da981fb7dbdcbfba
Aiden Harrison <100550195+asharrison97@users.noreply.github.com>
2025-05-04 20:21:43 -0500

    Added javadoc code to ExoticAnimalAdapter.java

--- src/main/java/adoptme/adapter/ExoticAnimalAdapter.java
+++ src/main/java/adoptme/adapter/ExoticAnimalAdapter.java
@@ -3,6 +3,12 @@ package adoptme.adapter;
 import adoptme.pet.*;
 
 public class ExoticAnimalAdapter {
+	/**
+	 * Method to adapt exotic animal information into a proper exotic
+	 * animal object.
+	 * @param raw - Data used to create the exotic animal object.
+	 * @return
+	 */
     public ExoticAnimal adapt(RawExoticAnimal raw) {
         return new ExoticAnimal(
             raw.uniqueId,
